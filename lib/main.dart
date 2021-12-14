40c29275532ea9b8a3c625aa5320cc094b9d14a1
HhmedHesham <heshama7793@gmail.com>
2021-12-14 20:22:51 +0200

    advanced search need to be modified RESOCODER

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 void main() => runApp(const MySearchApp());
 
@@ -7,30 +8,138 @@ class MySearchApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const historyLength = 5;
+
+  List<String> searchHistory = [
+    "hello there",
+    "sorry I cant",
+    "no proplem with this",
+    "hell yeaah",
+    "dad",
+    "dd",
+    "d",
+    "lol"
+  ];
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+  List<String>? filteredSearchHistory;
+  FloatingSearchBarController? controller;
+  String? selectedTerm;
+
+// filter the terms that start with the "filter" string
+  List<String> filterSearchTerms({@required String? filter}) {
+    if (filter != null && filter.isNotEmpty) {
+      // if filters contain more than letter then return the terms that started with thoses letters
+      return searchHistory.reversed
+          .where((term) => term.startsWith(filter))
+          .toList();
+    } else {
+      // else return the the whole search history
+      return searchHistory.reversed.toList();
+    }
+  }
+
+// implement the least recently used algorithm
+  void addSearchTerm(String term) {
+    if (searchHistory.contains(term)) {
+      putSearchTermFirst(term);
+      return;
+    }
+    searchHistory.add(term);
+    if (searchHistory.length > historyLength) {
+      searchHistory.removeRange(0, searchHistory.length - historyLength);
+    }
+    // we change the
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  void deleteSearchTerm(String term) {
+    searchHistory.removeWhere((t) => t == term);
+    // update the filteredSearchHistory
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  void putSearchTermFirst(String term) {
+    deleteSearchTerm(term);
+    addSearchTerm(term);
+  }
+
+  @override
+  void initState() {
+    // reversed search history
+    controller = FloatingSearchBarController();
+
+    filteredSearchHistory = filterSearchTerms(filter: null);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                // give it the context and SearchDelgateClass u will create
-                showSearch(
-                  context: context,
-                  delegate: CustomSearchDelegate(),
-                );
-              },
-              icon: Icon(Icons.search)),
-        ],
+    return FloatingSearchBar(
+      controller: controller,
+      body: SearchResultsListView(
+        searchTerm: selectedTerm,
+      ),
+      builder: (BuildContext context, Animation<double> transition) {
+        return Container();
+      },
+    );
+  }
+}
+
+class SearchResultsListView extends StatelessWidget {
+  final String? searchTerm;
+
+  const SearchResultsListView({
+    Key? key,
+    @required this.searchTerm,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (searchTerm == null) {
+      return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.search,
+              size: 64,
+            ),
+            Text(
+              'Start searching',
+              style: Theme.of(context).textTheme.headline5,
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: List.generate(
+        50,
+        (index) => ListTile(
+          title: Text('$searchTerm search result'),
+          subtitle: Text(index.toString()),
+        ),
       ),
     );
   }
@@ -48,6 +157,7 @@ class CustomSearchDelegate extends SearchDelegate {
     "d",
     "lol"
   ];
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
